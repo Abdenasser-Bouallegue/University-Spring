3c40f5b356998473f25a10f4cbbd1db69ab28d46
unknown <abdenasserbouallegue@gmail.com>
2023-11-05 21:04:33 +0100

    Add

--- src/main/java/tn/esprit/foyer/controller/FoyerController.java
+++ src/main/java/tn/esprit/foyer/controller/FoyerController.java
@@ -1,8 +1,11 @@
 package tn.esprit.foyer.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tn.esprit.foyer.entities.Etudiant;
+import tn.esprit.foyer.entities.Foyer;
 import tn.esprit.foyer.services.IFoyerService;
 
 @RestController
@@ -10,4 +13,8 @@ import tn.esprit.foyer.services.IFoyerService;
 @RequestMapping("Foyer")
 public class FoyerController {
     IFoyerService foyerService;
+    @GetMapping("/getAllFoyers")
+    public Iterable<Foyer> getAllFoyers(){
+        return foyerService.retrieveAllFoyers();
+    }
 }
--- src/main/java/tn/esprit/foyer/controller/UniversityController.java
+++ src/main/java/tn/esprit/foyer/controller/UniversityController.java
@@ -1,8 +1,8 @@
 package tn.esprit.foyer.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.foyer.entities.University;
 import tn.esprit.foyer.services.IUniversityService;
 
 @RestController
@@ -10,4 +10,29 @@ import tn.esprit.foyer.services.IUniversityService;
 @RequestMapping("University")
 public class UniversityController {
     IUniversityService universityService;
+    @GetMapping("/getallU")
+    public  Iterable<University> getAlluniversitys()
+    {
+        return universityService.retrieveAllUniversities();
+    }
+
+    @PostMapping("/add")
+    public  University addEtudiant(@RequestBody University u )
+    {
+        University university = universityService.addUniversity(u);
+        return u ;
+    }
+
+    @PutMapping("/update")
+    public  University updateuniversity(@RequestBody University u )
+    {
+        University university = universityService.updateUniversity(u);
+        return  u;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteuniversity(@PathVariable("id") long universityId)
+    {
+        universityService.removeUniversity(universityId);
+    }
 }
